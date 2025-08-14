bbf7ed5933c584a4f4f4d1134d27709d466a0798
sploj <109814290+sploj@users.noreply.github.com>
2025-08-14 23:18:07 +0100

    Update dart.gd

--- dart.gd
+++ dart.gd
@@ -29,7 +29,7 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: RigidBody2D) -> void:
 
 	if body.mob_type == "goblin":
-		print(body.health)
+		print("goblin hit")
 		goblin_hit(body)
 
 func goblin_hit(body):
