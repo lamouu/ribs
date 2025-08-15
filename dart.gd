21af5458b50c4ef7494bf326875c474805d6258e
lamou <69391156+lamouu@users.noreply.github.com>
2025-08-15 18:51:56 +0100

    dart knockback

--- dart.gd
+++ dart.gd
@@ -7,16 +7,18 @@ signal goblin_death
 @export var damage = 50
 @export var weapon_spacing = 60
 @export var char_offset = 35
+@export var knockback_impulse = 30000
 
 var velocity
 var player_node
+var firing_vec
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	set_as_top_level(true)
 	player_node = get_node("../../Player")
 	
-	var firing_vec = (get_viewport().get_mouse_position() - (get_viewport().get_visible_rect().size / 2) + Vector2(0, char_offset)).normalized()
+	firing_vec = (get_viewport().get_mouse_position() - (get_viewport().get_visible_rect().size / 2) + Vector2(0, char_offset)).normalized()
 	global_position = player_node.position - Vector2(0, char_offset) + firing_vec * weapon_spacing
 	rotation = firing_vec.angle()
 	velocity = firing_vec * speed
@@ -40,6 +42,5 @@ func goblin_hit(body):
 		player_node.score += 1
 		body.free()
 	else:
-		#body.apply_impulse(-direction * knockback_impulse)
-		pass
+		body.apply_impulse(firing_vec * knockback_impulse)
 		# flash body red using canvasmodulate
