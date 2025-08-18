619ff420a8380d00c2b2aa11b0ae73a4574f3e30
sploj <109814290+sploj@users.noreply.github.com>
2025-08-18 23:49:44 +0100

    fixed enemy spawns
    enemies now spawn outside of the screen, but inside of the map, even when the camera is near the edge of the map

--- main.gd
+++ main.gd
@@ -2,7 +2,7 @@ extends Node
 
 @export var goblin_resource: Resource
 @export var goblin_base_scene: PackedScene
-var player_camera
+var player_camera_size
 var screen_size
 var rng = RandomNumberGenerator.new()
 var spawn_buffer_y: float
@@ -16,9 +16,9 @@ func _ready() -> void:
 	$SpawnTimer.start()
 	$Player.position = $StartPosition.position
 	screen_size = $'/root'.size
-	player_camera = get_viewport()
-	spawn_buffer_y = (player_camera.size.y / 3)
-	spawn_buffer_x = (player_camera.size.x / 3)
+	player_camera_size = get_viewport().size
+	spawn_buffer_y = (player_camera_size.y / 3)
+	spawn_buffer_x = (player_camera_size.x / 3)
 	
 	#base goblin spawn test
 	#var goblin_base = goblin_base_scene.instantiate()
@@ -41,18 +41,18 @@ func _on_spawn_timer_timeout() -> void:
 	
 	var map_top_position = $Map.position.y
 	var map_bottom_position = map_top_position + $Map.size.y
-	var player_camera_top_position = camera_center.y - (screen_size.y / 2)
-	var player_camera_bottom_position = camera_center.y + (screen_size.y / 2)
+	var player_camera_top_position = camera_center.y - (player_camera_size.y / 2)
+	var player_camera_bottom_position = camera_center.y + (player_camera_size.y / 2)
 	var map_leftmost_position = $Map.position.x
 	var map_rightmost_position = map_leftmost_position + $Map.size.x
-	var player_camera_leftmost_position = camera_center.x - (screen_size.x / 2)
-	var player_camera_rightmost_position = camera_center.x + (screen_size.x / 2)
+	var player_camera_leftmost_position = camera_center.x - (player_camera_size.x / 2)
+	var player_camera_rightmost_position = camera_center.x + (player_camera_size.x / 2)
 	
 	#gets random x and y coordinates outside of the screen
-	var random_above_screen_vertical:float = randf_range((map_top_position + spawn_buffer_y) , player_camera_top_position)
-	var random_below_screen_vertical:float = randf_range((map_bottom_position - spawn_buffer_y) , player_camera_bottom_position)
-	var random_left_of_screen_horizontal: float = randf_range((map_leftmost_position + spawn_buffer_x), player_camera_leftmost_position)
-	var random_right_of_screen_horizontal: float = randf_range((map_rightmost_position - spawn_buffer_x), player_camera_rightmost_position)
+	var random_above_screen_vertical:float = randf_range((map_top_position + spawn_buffer_y) , player_camera_top_position * 1.1)
+	var random_below_screen_vertical:float = randf_range((map_bottom_position - spawn_buffer_y) , player_camera_bottom_position * 1.1)
+	var random_left_of_screen_horizontal: float = randf_range((map_leftmost_position + spawn_buffer_x), player_camera_leftmost_position * 1.1)
+	var random_right_of_screen_horizontal: float = randf_range((map_rightmost_position - spawn_buffer_x), player_camera_rightmost_position * 1.1)
 	
 	#gets random x and y coordinates from screen size
 	var random_y_coordinate = randf_range(map_top_position, map_bottom_position)
@@ -61,15 +61,31 @@ func _on_spawn_timer_timeout() -> void:
 	
 	
 	#if the spawn location it outside the map, spawns on the opposite side of the screen
-	if random_above_screen_vertical < map_top_position:
-		mob_spawn_zone = "below"
-	if random_below_screen_vertical > map_bottom_position:
-		mob_spawn_zone = "above"
 	
-	if random_left_of_screen_horizontal < map_leftmost_position:
-		mob_spawn_zone = "right"
-	if random_right_of_screen_horizontal > map_rightmost_position:
-		mob_spawn_zone = "left"
+	
+	if mob_spawn_zone == "above":
+		if random_above_screen_vertical > player_camera_top_position:
+			mob_spawn_zone = "below"
+		if random_above_screen_vertical < map_top_position:
+			mob_spawn_zone = "below"
+	
+	if mob_spawn_zone == "below":
+		if random_below_screen_vertical < player_camera_bottom_position:
+			mob_spawn_zone = "above"
+		if random_below_screen_vertical > map_bottom_position:
+			mob_spawn_zone = "above"
+	
+	if mob_spawn_zone == "right":
+		if random_right_of_screen_horizontal < player_camera_rightmost_position:
+			mob_spawn_zone = "left"
+		if random_right_of_screen_horizontal > map_rightmost_position:
+			mob_spawn_zone = "left"
+	
+	if mob_spawn_zone == "left":
+		if random_left_of_screen_horizontal > player_camera_leftmost_position:
+			mob_spawn_zone = "right"
+		if random_left_of_screen_horizontal < map_leftmost_position:
+			mob_spawn_zone = "right"
 	
 	#combines a random x and random y coordinate to create a spawn location
 	match mob_spawn_zone:
@@ -91,6 +107,7 @@ func _on_spawn_timer_timeout() -> void:
 	print("the bottom of the map: ", map_bottom_position)
 	print(camera_center)
 	print(screen_size)
+	print(player_camera_size)
 	
 	add_child(mob)
 	
