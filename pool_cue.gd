3cd92b61b59b32135967db0de008785e80b2dce4
sploj <109814290+sploj@users.noreply.github.com>
2025-08-20 03:41:46 +0100

    pool cue improvements
    massively improved pool cue animation and set fire to right click release, before releasing the pool cue now rotates to follow the mouse

--- pool_cue.gd
+++ pool_cue.gd
@@ -3,22 +3,51 @@ extends Area2D
 @export var knockback_impulse = 90000
 
 var movement: Vector2
-var speed = 5
-var char_offset = 5
+var speed = 15
+var char_offset = 75
+var weapon_spacing = 125
 var firing_vec
 var collision_type = "pool_cue"
+var rotates_with_mouse = true
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	firing_vec = (get_viewport().get_mouse_position() - (get_viewport().get_visible_rect().size / 2) + Vector2(0, char_offset)).normalized()
-	rotation = firing_vec.angle()
-	movement = firing_vec * speed
+	position -= Vector2(0, 50)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	global_position += movement
+	if rotates_with_mouse == true:
+		rotate_to_mouse()
+		if Input.is_action_just_released("special_attack"):
+			speed = 20
+			movement = firing_vec * speed
+			rotates_with_mouse = false
+			$CollisionShape2D.set_deferred("disabled", false)
+			$CueExtendTimer.start()
+
+	if rotates_with_mouse == false:
+		position += movement
+	#if is_firing == false:
+	#	if is_returning == false:
+	#		#position -= Vector2(0, char_offset) - firing_vec * weapon_spacing
+	#		firing_vec = (get_viewport().get_mouse_position() - (get_viewport().get_visible_rect().size / 2) + Vector2(0, char_offset)).normalized()
+	#		rotation = firing_vec.angle()
 	
+	
+	#if is_firing == true:
+	#	global_position += firing_vec * movement
+	#if is_returning == true:
+	#	global_position += firing_vec * movement
+		
+	
+
+func rotate_to_mouse():
+	firing_vec = (get_viewport().get_mouse_position() - (get_viewport().get_visible_rect().size / 2) + Vector2(0, char_offset)).normalized()
+	rotation = firing_vec.angle()
+	position = (firing_vec * weapon_spacing) - Vector2(0, 70)
+	if visible == false:
+		visible = true
 
 func _on_cue_retract_timer_timeout() -> void:
 	queue_free()
@@ -27,5 +56,8 @@ func disable_collision():
 		$CollisionShape2D.set_deferred("disabled", true)
 
 func _on_cue_extend_timer_timeout() -> void:
+	$CueRetractTimer.start()
+	speed = 3
 	movement = -firing_vec * speed
 	$CollisionShape2D.set_deferred("disabled", true)
+	
