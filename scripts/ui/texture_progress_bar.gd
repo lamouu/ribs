d38adbdf3bf5a6cf3cfc91b9c18fe0ad37dfb3b8
lamou <69391156+lamouu@users.noreply.github.com>
2025-08-20 02:54:25 +0100

    new ui

--- scripts/ui/texture_progress_bar.gd
+++ scripts/ui/texture_progress_bar.gd
@@ -11,3 +11,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	value = roundtimer.time_left
+	print(value)
