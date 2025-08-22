411b6a1dc4dd1f3323ca7d106c37cc5832eaeebe
lamou <69391156+lamouu@users.noreply.github.com>
2025-08-22 23:32:11 +0100

    shop working

--- scripts/ui/shops/gnome_shop.gd
+++ scripts/ui/shops/gnome_shop.gd
@@ -1,12 +1,42 @@
 extends Control
 
+signal inventory_updated
+
+var player_node
+var available_items = Items.available_items
+var shop_items: Dictionary
+
+func _ready() -> void:
+	player_node = get_node("/root/Node/Main/Player")
+
 func roll_items():
-	var available_items = Items.available_items.duplicate()
-	var shop_items: Dictionary
+	available_items = Items.available_items.duplicate()
+	shop_items = {}
 	
 	for item_button in [$Item1, $Item2, $Item3]:
 		var rolled_item = available_items.keys().pick_random()
+		print(available_items)
 		
-		item_button.texture_normal = available_items[rolled_item].texture
+		shop_items[item_button] = available_items[rolled_item]
+		item_button.texture_normal = shop_items[item_button].texture
 		
 		available_items.erase(rolled_item)
+
+
+func _on_item_1_pressed() -> void:
+	player_node.inventory.add_item(shop_items[$Item1])
+	player_node.inventory_updated.emit()
+
+
+func _on_item_2_pressed() -> void:
+	player_node.inventory.add_item(shop_items[$Item2])
+	player_node.inventory_updated.emit()
+
+
+func _on_item_3_pressed() -> void:
+	player_node.inventory.add_item(shop_items[$Item3])
+	player_node.inventory_updated.emit()
+
+
+func _on_refresh_pressed() -> void:
+	roll_items()
